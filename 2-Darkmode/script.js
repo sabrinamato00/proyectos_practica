1e491cdb51b067e5bd248981c5718bd23d171d75
sabirnamato00 <sabrinamato00@gmail.com>
2023-04-14 00:01:43 -0300

    commit darkmode

--- 2-Darkmode/script.js
+++ 2-Darkmode/script.js
@@ -1,1 +1,9 @@
-    
+const btnSwitch = document.querySelector("#switch");
+// const card = document.getElementsByClassName("#card");
+// const header = document.querySelector("header");
+
+btnSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
+  //   header.classList.toggle("dark");
+});
